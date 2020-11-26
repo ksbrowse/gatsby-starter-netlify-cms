9f66634829e82bdb6ea51c2b6bd75fd2e7416bce
ksbrowse <ks.browse@gmail.com>
2020-11-26 20:45:55 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -35,6 +35,11 @@ const Footer = class extends React.Component {
                       </Link>
                     </li>
                     <li>
+                      <Link className="navbar-item" to="/brewing">
+                        Brewing
+                      </Link>
+                    </li>
+                    <li>
                       <Link className="navbar-item" to="/products">
                         Products
                       </Link>
